0a253a99a17e230cac867a23bb2d9e86d6aa9276
HYJ <tpdl5832@naver.com>
2023-03-18 17:12:08 +0900

    input type이 search일 경우 x 버튼 없애는 css 추가

--- src/common/input.js
+++ src/common/input.js
@@ -3,13 +3,25 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Input = styled.input`
-display : ${(props) => (props.type === 'search' ? 'inline-flex;' : 'flex;')}
+  display : ${(props) => (props.type === 'search' ? 'inline-flex;' : 'flex;')}
   padding-left: 5px;
   border-radius: 5px;
   width: 100%;
   height: 40px;
   margin-top: 20px;
   border: ${(props) => props.border};
+  
+  //x버튼 없애기
+  ::-ms-clear,
+  ::-ms-reveal{
+    display:none;width:0;height:0;
+  }
+  ::-webkit-search-decoration,
+  ::-webkit-search-cancel-button,
+  ::-webkit-search-results-button,
+  ::-webkit-search-results-decoration{
+    display:none;
+  }
 `;
 const input = (props) => {
   const { type, placeholder, border, onChangeFunc, value } = props;
